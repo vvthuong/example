00985522f81ebaf6b6f770faa68cdaed4c83c733
Your Name <you@example.com>
2022-03-02 12:30:51 +0700

    hihi

--- main.go
+++ main.go
@@ -3,5 +3,6 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("hello... hihi")
+	he := Haha()
+	fmt.Println(he)
 }
---
+++ name.go
@@ -0,0 +1,5 @@
+package main
+
+func Haha() string {
+	return "Hello World..."
+}
